a53f386b0007b3563e69df6a03493fa5f0d2e172
MatheusRQueiroz <mq31211@gmail.com>
2022-07-23 11:05:52 -0300

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'components/campo_pesquisa.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,61 +18,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Mercado Aberto'),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class CampoPesquisa extends StatefulWidget {
-  const CampoPesquisa({ Key? key }) : super(key: key);
-
-  @override
-  _CampoPesquisaState createState() => _CampoPesquisaState();
-}
-
-class _CampoPesquisaState extends State<CampoPesquisa> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-  width: MediaQuery.of(context).size.width - 110,
-            padding: const EdgeInsets.only(top: 10),
-            child: TextField(
-              onSubmitted: (test) => {},
-              decoration: const InputDecoration(
-                isDense: true,
-                contentPadding: EdgeInsets.only(top: 5),
-                filled: true,
-                fillColor: Colors.white,
-                hintText: "Buscar no Mercado Aberto",
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0xffffdb15),
-                    width: 0,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30.0),
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0xffffdb15),
-                    width: 0,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30.0),
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0xffffdb15),
-                    width: 0,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30.0),
-                  ),
-                ),
-              ),
-            ),      
     );
   }
 }
